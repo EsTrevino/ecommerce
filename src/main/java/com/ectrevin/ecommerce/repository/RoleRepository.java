8840f28498f76a107035c98762a58598b9e85ca6
EsTrevino <sttrevino2@gmail.com>
2018-10-12 09:12:27 -0700

    fixed username error in repository code

--- src/main/java/com/ectrevin/ecommerce/repository/RoleRepository.java
+++ src/main/java/com/ectrevin/ecommerce/repository/RoleRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName roleName);
 
-    
+
 
 
 }
--- src/main/java/com/ectrevin/ecommerce/repository/UserRepository.java
+++ src/main/java/com/ectrevin/ecommerce/repository/UserRepository.java
@@ -17,7 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
-    Boolean existsByUserName(String username);
+    Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
 }
