c3c6d2a62301bf9932cb51b42103eaca6cf3b1e7
EsTrevino <sttrevino2@gmail.com>
2018-10-12 08:18:53 -0700

    added model package for User, Role models. Created auditConfig class for JpaAuditing. Created repository package for User and Role interfaces

---
+++ src/main/java/com/ectrevin/ecommerce/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.ectrevin.ecommerce.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/ectrevin/ecommerce/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.ectrevin.ecommerce.repository;
+
+import com.ectrevin.ecommerce.model.Role;
+import com.ectrevin.ecommerce.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+
+    
+
+
+}
